f64364edc10720fad480e3c05061e613e45044ff
tejasrana1 <tejasrana7771@gmail.com>
2022-07-01 17:21:36 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import Login from './components/Login';
-
+import Register from './components/Register';
 function App() {
   return (
     <div className="App">
-      <Login />
+      <Register />
     </div>
   );
 }
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,8 +1,168 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { TextField,Box,Button } from '@mui/material'
+import { passwordReGex, usernameReGex } from './reGex'
+import { login,selectUser } from '../store/userSlice'
+import { useDispatch, useSelector } from 'react-redux'
+
 
 const Register = () => {
+    const userData = useSelector(selectUser)
+    const dispatch = useDispatch()
+    const [user,setUser] = useState({
+        name: "",
+        email: "",
+        eid: "",
+        phone: ""
+    })
+    const [err,setErr] = useState({
+        name: false,
+        email: false,
+        eid: false,
+        phone: false
+    })
+    const [msg,setMsg] = useState({
+        name: "",
+        email: "",
+        eid: "",
+        phone: ""
+    })
+    function handleSubmit(e){
+        e.preventDefault()
+       alert("hi")
+       console.log(user);
+       dispatch(login({user}))
+    }
+    const handleChange = (e)=>{
+        const {name,value} = e.target;
+        setUser(prev=>{
+            return{
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+    const validate = (e)=>{
+        if(e.target.name === "password"){
+        if(!passwordReGex.test(user.password)){
+            setErr(prev=>{
+                return{
+                    ...prev,
+                    password: true
+                }
+            })
+            setMsg(prev=>{
+                return{
+                    ...prev,
+                    password: "Password Validation Failed"
+                }
+            })
+        }
+        else{
+            setErr(prev=>{
+                return{
+                    ...prev,
+                    password: false
+                }
+            })
+            setMsg(prev=>{
+                return{
+                    ...prev,
+                    password: ""
+                }
+            })
+        }
+    }
+        if(e.target.name === "username"){
+        if(!usernameReGex.test(user.username)){
+            setErr(prev=>{
+                return{
+                    ...prev,
+                    username: true
+                }
+            })
+            setMsg(prev=>{
+                return{
+                    ...prev,
+                    username: "Username Validation Failed"
+                }
+            })
+        }
+        else{
+            setErr(prev=>{
+                return{
+                    ...prev,
+                    username: false
+                }
+            })
+            setMsg(prev=>{
+                return{
+                    ...prev,
+                    username: ""
+                }
+            })
+        }
+    }
+    }
   return (
-    <div>Register</div>
+    <div>
+        <button onClick={()=>{console.log(userData);}}>a</button>
+         <Box
+         onSubmit={handleSubmit}
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '50ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+      <TextField
+          error={err.name}
+          id="outlined-error-helper-text"
+          label="Name"
+          helperText={msg.name}
+          value={user.name}
+          onChange={handleChange}
+          onBlur={validate}
+          name="name"
+        />
+        <TextField
+          error={err.email}
+          id="outlined-error-helper-text"
+          type="email"
+          label="Email"
+          helperText={msg.email}
+          value={user.email}
+          onChange={handleChange}
+          onBlur={validate}
+          name="email"
+        />
+        <TextField
+          error={err.phone}
+          id="outlined-error-helper-text"
+          type="tel"
+          label="Phone"
+          helperText={msg.phone}
+          value={user.phone}
+          onChange={handleChange}
+          onBlur={validate}
+          name="phone"
+        />
+        <TextField
+          error={err.eid}
+          id="outlined-error-helper-text"
+          type="number"
+          label="Employee Id"
+          helperText={msg.eid}
+          value={user.eid}
+          onChange={handleChange}
+          onBlur={validate}
+          name="eid"
+        />
+        <Button disabled={err.password && err.username} variant="contained" type='submit'>Submit</Button>
+      </div>
+      </Box>
+    </div>
   )
 }
 
